e0c79cd11cd79a9f77d9383df7752706fe0d84c9
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-15 21:42:28 +0900

    :memo: #2.3 | AppLoading part Three

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
 import AppLoading from "expo-app-loading";
-import React, { useState } from "react";
-import * as Font from "expo-font";
-import { Asset } from "expo-asset";
-import { Text, Image } from "react-native";
+import React from "react";
+import { useFonts } from "expo-font";
+import { useAssets } from "expo-asset";
+import { Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) => {
-  return images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-};
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./my-face.png"),
-      "https://reactnative.dev/img/oss_logo.png",
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.log}
-      />
-    );
+  const [assets] = useAssets([require("./my-face.png")]);
+  const [loaded] = useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done loading!</Text>;
 }
