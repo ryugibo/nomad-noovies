e22de79ee3c9cf22e1772ba15d831c13f5d509ff
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-16 17:57:29 +0900

    :memo: #2.8 | Tab Bar Icons
    - navigation theme

--- App.js
+++ App.js
@@ -4,16 +4,23 @@ import { useFonts } from "expo-font";
 import { useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 import Tabs from "./navigation/Tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { useColorScheme } from "react-native";
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
 
 export default function App() {
   const [assets] = useAssets([require("./my-face.png")]);
   const [loaded] = useFonts(Ionicons.font);
+  const isDark = useColorScheme() === "dark";
+
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
       <Tabs />
     </NavigationContainer>
   );
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,23 +3,12 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-import { useColorScheme } from "react-native";
-import { YELLOW_COLOR, BLACK_COLOR, DARK_GREY, LIGHT_GREY } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-  const isDark = useColorScheme() === "dark";
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
-        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
-        tabBarInactiveTintColor: isDark ? LIGHT_GREY : DARK_GREY,
-        headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
-        headerTitleStyle: { color: isDark ? "white" : BLACK_COLOR },
-      }}
-    >
+    <Tab.Navigator>
       <Tab.Screen name="Movies" component={Movies} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
