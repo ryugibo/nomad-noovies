dd00484092e4caf73aca8cba0917396163de8b13
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-16 17:58:23 +0900

    :memo: #2.8 | Tab Bar Icons
    - reset

--- App.js
+++ App.js
@@ -4,23 +4,16 @@ import { useFonts } from "expo-font";
 import { useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 import Tabs from "./navigation/Tabs";
-import { useColorScheme } from "react-native";
-import {
-  NavigationContainer,
-  DarkTheme,
-  DefaultTheme,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   const [assets] = useAssets([require("./my-face.png")]);
   const [loaded] = useFonts(Ionicons.font);
-  const isDark = useColorScheme() === "dark";
-
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+    <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
