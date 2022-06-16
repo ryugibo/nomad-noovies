f086d0dbb1c327a7f04fd3100b06ce01313f0459
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-17 01:15:58 +0900

    :memo: #2.15 | Themes

--- App.js
+++ App.js
@@ -5,16 +5,22 @@ import { useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./navigation/Root";
+import { useColorScheme } from "react-native";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
   const [assets] = useAssets([require("./my-face.png")]);
   const [loaded] = useFonts(Ionicons.font);
+  const isDark = useColorScheme() === "dark";
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
