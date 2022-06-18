acb3bef7fa1ce492451c11b2bc51dd581442929a
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-18 17:48:44 +0900

    :memo: #3.2 | Movies Screen part One

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,6 +1,42 @@
 import React from "react";
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Swiper from "react-native-web-swiper";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => null;
+const API_KEY = "3c983d1e60d94e03820760af3fae791e";
+
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&region=KR&language=ko-KR`
+    );
+  };
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "tomato" }}></View>
+        <View style={{ backgroundColor: "teal" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movies;
