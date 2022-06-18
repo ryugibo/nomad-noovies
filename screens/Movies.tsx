246283cfe2a36e619bbdefe400b3a20ff4d57a37
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-18 18:37:14 +0900

    :memo: #3.4 | Movies Screen part Three

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -16,20 +16,44 @@ import { BlurView } from "expo-blur";
 const API_KEY = "3c983d1e60d94e03820760af3fae791e";
 
 const Container = styled.ScrollView``;
-
 const View = styled.View`
   flex: 1;
 `;
-
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-
 const BgImg = styled(Image)``;
-
-const Title = styled(Text)``;
+const Poster = styled(Image)`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+const Title = styled(Text)`
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+`;
+const Wrapper = styled(View)`
+  flex-direction: row;
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+`;
+const Column = styled(View)`
+  width: 60%;
+  margin-left: 15px;
+`;
+const Overview = styled(Text)`
+  margin-top: 10px;
+  color: rgba(255, 255, 255, 0.8);
+`;
+const Vote = styled(Overview)`
+  font-size: 12px;
+`;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -58,9 +82,12 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.5}
-        controlsEnabled={false}
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlayingMovies.map((movie) => (
@@ -74,7 +101,16 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
               intensity={80}
               style={StyleSheet.absoluteFill}
             >
-              <Title>{movie.original_title}</Title>
+              <Wrapper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title>{movie.title}</Title>
+                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
+                  {movie.vote_average > 0 && (
+                    <Vote>⭐{movie.vote_average}/10</Vote>
+                  )}
+                </Column>
+              </Wrapper>
             </BlurView>
           </View>
         ))}
