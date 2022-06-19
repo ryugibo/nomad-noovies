f2bdf8a568bec715fe51e9f8e567825308e97565
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-19 12:08:55 +0900

    :memo: #3.6 | Trending Movies

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, ActivityIndicator } from "react-native";
+import {
+  Dimensions,
+  ActivityIndicator,
+  Text,
+  ScrollView,
+  View,
+  useColorScheme,
+} from "react-native";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import Swiper from "react-native-swiper";
 import Slide from "../components/Slide";
+import Poster from "../components/Poster";
 
 const API_KEY = "3c983d1e60d94e03820760af3fae791e";
 
@@ -13,9 +21,35 @@ const Loader = styled.View`
   justify-content: center;
   align-items: center;
 `;
+const ListTitle = styled(Text)<{ isDark: boolean }>`
+  color: ${(props) => (props.isDark ? "white" : "black")};
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+const Movie = styled(View)`
+  margin-right: 20px;
+  align-items: center;
+`;
+const TrendingScroll = styled(ScrollView)`
+  margin-top: 20px;
+`;
+const Title = styled(Text)<{ isDark: boolean }>`
+  color: ${(props) => (props.isDark ? "white" : "black")};
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+  font-size: 8px;
+`;
+const Vote = styled(Text)<{ isDark: boolean }>`
+  color: ${(props) => (props.isDark ? "white" : "black")};
+  font-size: 10px;
+`;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
   const [upcoming, setUpcoming] = useState([]);
@@ -23,7 +57,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const getTrending = async () => {
     const { results } = await (
       await fetch(
-        `https://api.themoviedb.org/3/movie/trending/movie/week?api_key=${API_KEY}&region=KR&language=ko-KR`
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&region=KR&language=ko-KR`
       )
     ).json();
 
@@ -69,7 +103,11 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         autoplayTimeout={3.5}
         showsButtons={false}
         showsPagination={false}
-        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+        containerStyle={{
+          width: "100%",
+          height: SCREEN_HEIGHT / 4,
+          marginBottom: 30,
+        }}
       >
         {nowPlayingMovies.map((movie) => (
           <Slide
@@ -82,6 +120,25 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
           />
         ))}
       </Swiper>
+      <ListTitle isDark={isDark}>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title isDark={isDark}>
+              {movie.title.slice(0, 13)}
+              {movie.title.length > 13 ? "..." : ""}
+            </Title>
+            {movie.vote_average > 0 ? (
+              <Vote isDark={isDark}>⭐{movie.vote_average}/10</Vote>
+            ) : null}
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
