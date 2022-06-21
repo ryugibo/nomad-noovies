27dc6daedbe7c9fc9bfbf0f6325372300551185c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-21 20:13:17 +0900

    :memo: #3.23 | Detail Screen part Three

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,13 +1,46 @@
 import React, { useEffect } from "react";
-import styled from "styled-components/native";
-import { ScrollView, Text } from "react-native";
+import styled, { useTheme } from "styled-components/native";
+import {
+  ScrollView,
+  View,
+  Image,
+  Dimensions,
+  StyleSheet,
+  Text,
+} from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Movie, TV } from "../api";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../utils";
+import { LinearGradient } from "expo-linear-gradient";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled(ScrollView)`
   background-color: ${(props) => props.theme.mainBgColor};
   color: ${(props) => props.theme.textColor};
+`;
+const Header = styled(View)`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+`;
+const Background = styled(Image)``;
+const Column = styled(View)`
+  flex-direction: row;
+  width: 80%;
+`;
+const Title = styled(Text)`
+  color: ${(props) => props.theme.textColor};
+  font-size: 36px;
+  align-self: flex-end;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+const Overview = styled(Text)`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0px 20px;
 `;
 
 type RootStackParamList = {
@@ -20,12 +53,27 @@ const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params },
 }) => {
+  const theme = useTheme();
   useEffect(() => {
-    setOptions({ title: "title" in params ? params.title : params.name });
+    setOptions({ title: "title" in params ? "Movie" : "TV Show" });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || null) }}
+        />
+        <LinearGradient
+          colors={["transparent", theme.mainBgColor]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>{"title" in params ? params.title : params.name}</Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
